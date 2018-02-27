05e18cb291ea2f06da02d753bedf2a38fe77caae
mfrancomartinez <mfrancomartinez@victoria17.danielcastelao.org>
2018-02-27 09:00:49 +0100

    Boletin_4_2

---
+++ Boletin_4_2/src/boletin_4_2/Boletin_4_2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_4_2;
+
+/**
+ *
+ * @author mfrancomartinez
+ */
+public class Boletin_4_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Satelite ex1 = new Satelite();
+    ex1.verPosicion();
+    }
+    
+}

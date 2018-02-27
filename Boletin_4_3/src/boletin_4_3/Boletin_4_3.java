eb4a75d39ed95712766d9dfd7e6a71e5d272f995
mfrancomartinez <mfrancomartinez@victoria17.danielcastelao.org>
2018-02-27 09:12:48 +0100

    Boletin_4_3

---
+++ Boletin_4_3/src/boletin_4_3/Boletin_4_3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_4_3;
+
+/**
+ *
+ * @author mfrancomartinez
+ */
+public class Boletin_4_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Circulo ex1 = new Circulo();
+    ex1.setRadio(50);
+    ex1.calcularArea();
+    ex1.calcularLonxitude();
+    }
+    
+}
